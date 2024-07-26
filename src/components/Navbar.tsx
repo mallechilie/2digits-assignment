82fd2a06b53418c806c453266fd27b989dac9254
Michiel <michiel.doorn@gmail.com>
2024-07-26 17:14:16 +0200

    Add navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,31 @@
 import * as React from 'react';
 
+import Link from 'next/link';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
-const pages = ['Home', 'Blogs'];
+const pages = [
+  { name: 'Home', page: '/' },
+  { name: 'Blogs', page: 'blogs' },
+];
 
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, mb: '1px' }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            2DIGITS
+            <Link href="/">2DIGITS</Link>
           </Typography>
 
           <Box sx={{ display: 'flex' }}>
             {pages.map((page) => (
-              <Button key={page} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {page}
+              <Button key={page.name} sx={{ my: 2, color: 'white', display: 'block' }}>
+                <Link href={page.page}>{page.name}</Link>
               </Button>
             ))}
           </Box>
